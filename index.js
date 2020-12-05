989b8dc7d0dcdf246b71d9dc0a7bdb1403b17d15
Kenneth <kyleabs20@gmail.com>
2020-12-04 16:28:57 -0800

    facing errors

--- index.js
+++ index.js
@@ -2,12 +2,31 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
-const packageDef = protoLoader.loadSync("proto/todo.proto", {}); // TODO: Package definition
+const packageDef = protoLoader.loadSync("todo.proto", {}); // TODO: Package definition
 const grpcObject = grpc.loadPackageDefinition(packageDef); // TODO: loading the package as an object
-const todoPackage = grpc.todoPackage; // TODO getting the todopackage finally to get access to everything
+const todoPackage = grpcObject.todoPackage; // TODO getting the todopackage finally to get access to everything
 
-TODO // creating a grpc server
+// TODO creating a grpc server
 const server = new grpc.Server();
 server.bind('0.0.0.0:7000', grpc.ServerCredentials.createInsecure());
 
+// TODO giving your server a service 
+server.addService(todoPackage.Todo.service, 
+    {
+        "createTodo": createTodo,
+        "getAllTodos": getAllTodos
+    });
+
+// TODO starting the server
+server.start();
+
+// TODO creating individual functions for each rpc service 
+
+function createTodo(call, callback){
+    console.log(call);
+}
+
+function getAllTodos(call, callback){
+
+}
 
