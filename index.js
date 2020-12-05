76b1dff3b556c1ab8d21a1c42bf55e4d357604ab
Kenneth <kyleabs20@gmail.com>
2020-12-04 17:19:12 -0800

    no bi easy oooooo

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 // imports 
+var proto_path = require('./proto/todo.proto')
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
-const packageDef = protoLoader.loadSync("todo.proto", {}); // TODO: Package definition
+const packageDef = protoLoader.loadSync("proto/todo.proto", {}); // TODO: Package definition
 const grpcObject = grpc.loadPackageDefinition(packageDef); // TODO: loading the package as an object
-const todoPackage = grpcObject.todoPackage; // TODO getting the todopackage finally to get access to everything
+// TODO using a different approach
+const todoPackage = grpcObject.TodoPackage; // TODO getting the todopackage finally to get access to everything
 
 // TODO creating a grpc server
 const server = new grpc.Server();
+
+// TODO adding services 
+server.addService(todoPackage.Todo.service, {
+    "createTodo": createTodo, 
+    "getAllTodos": getAllTodos,
+});
+
 server.bind('0.0.0.0:7000', grpc.ServerCredentials.createInsecure());
-
-// TODO giving your server a service 
-server.addService(todoPackage.Todo.service, 
-    {
-        "createTodo": createTodo,
-        "getAllTodos": getAllTodos
-    });
-
-// TODO starting the server
 server.start();
 
-// TODO creating individual functions for each rpc service 
 
+TODO // functions for services
 function createTodo(call, callback){
-    console.log(call);
+
 }
 
 function getAllTodos(call, callback){
 
 }
+
 
